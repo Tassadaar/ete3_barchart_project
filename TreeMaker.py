83d20712251749b096a7a82c85efb9a6a1d0fa13
Jason Shao <qz228113@dal.ca>
2023-05-08 18:07:55 -0300

    created TreeMaker.py as an example of simple tree shown in GUI

---
+++ TreeMaker.py
@@ -0,0 +1,14 @@
+"""
+This program makes a simple unrooted tree from a newwick string, and show said tree in GUI.
+"""
+
+from ete3 import Tree
+
+
+# variable initiations
+newwick_tree = "input_files/nematode_CH.tree"
+
+# tree "growing"
+unrooted_tree = Tree(newwick_tree)
+# print(unrooted_tree)
+unrooted_tree.show()
