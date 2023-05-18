cfc1bae63899eb4a0fd9fa9779e0eb56c16fde3d
Jason Shao <qz228113@dal.ca>
2023-05-18 14:52:05 -0300

    updated TreeMaker.py to eliminate the need for FaceMaker

--- TreeMaker.py
+++ TreeMaker.py
@@ -3,9 +3,8 @@ This program makes a simple unrooted tree from a newwick string, and render said
 """
 
 import argparse
-from ete3 import Tree, faces, TreeStyle
+from ete3 import Tree, faces, TreeStyle, BarChartFace
 from FrequencyCalculator import FrequencyCalculator
-from FaceMaker import FaceMaker
 
 # specify tags
 parser = argparse.ArgumentParser(description="Tree making")
@@ -26,18 +25,27 @@ mode = args.mode
 taxa_list = FrequencyCalculator(file_location, file_format).get_taxa_list()
 
 # making faces
-faceMaker = FaceMaker()
+#faceMaker = FaceMaker()
 face_dict = {}
 fymink_dict = {}
 garp_dict = {}
 others_dict = {}
 
 for taxon in taxa_list:
-    face_dict[taxon.name] = faceMaker.make_barchartface(taxon.freq_dict)
-    fymink_dict[taxon.name] = faceMaker.make_barchartface(taxon.fymink_freq_dict)
-    garp_dict[taxon.name] = faceMaker.make_barchartface(taxon.garp_freq_dict)
-    others_dict[taxon.name] = faceMaker.make_barchartface(taxon.others_freq_dict)
+    #face_dict[taxon.name] = faceMaker.make_barchartface(taxon.freq_dict)
+    face_dict[taxon.name] = BarChartFace(
+        values=list(taxon.freq_dict.values()),
+        labels=list(taxon.freq_dict.keys()),
+        colors= ["blue" for key in taxon.freq_dict.keys()],
+        max_value = 0.2,
+        width=100,
+        height=50
+    )
+    #fymink_dict[taxon.name] = faceMaker.make_barchartface(taxon.fymink_freq_dict)
+    #garp_dict[taxon.name] = faceMaker.make_barchartface(taxon.garp_freq_dict)
+    #others_dict[taxon.name] = faceMaker.make_barchartface(taxon.others_freq_dict)
 
+print(fymink_dict)
 
 # tree "growing"
 tree = Tree(newwick_tree)
@@ -55,10 +63,11 @@ def layout_fn(node):
 
 
 def layout_sp(node):
-    if node.is_leaf():
+    if node.is_leaf(): # && node.name = "HONEYBEE":
         fymink_face = fymink_dict[node.name]
         garp_face = garp_dict[node.name]
         others_face = others_dict[node.name]
+        #print(fymink_face.values)
         faces.add_face_to_node(face=fymink_face, node=node, column=1, position="aligned")
         #faces.add_face_to_node(face=garp_face, node=node, column=2, position="aligned")
         #faces.add_face_to_node(face=others_face, node=node, column=3, position="aligned")
