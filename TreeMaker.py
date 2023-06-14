6f36970defe605c2761f52696a316d9475fedb63
Jason Shao <qz228113@dal.ca>
2023-06-14 16:36:46 -0300

    encapsulated code in TreeMaker.py under main()

--- TreeMaker.py
+++ TreeMaker.py
@@ -14,99 +14,100 @@ from Bio import SeqIO
 from Taxon import Taxon
 from ete3 import Tree, faces, TreeStyle, BarChartFace
 
-# specify tags
-parser = argparse.ArgumentParser(description="Tree making")
 
-parser.add_argument("-t", "--tree", required=True)
-parser.add_argument("-n", "--file", required=True)
-parser.add_argument("-f", "--format", required=True)
-parser.add_argument("-o", "--output", type=str, default="barchart_tree")
-parser.add_argument("-s", "--subset", type=str, default="none")
-parser.add_argument("-m", "--frequency_type", type=str, default="absolute")
+def main():
+    # specify tags
+    parser = argparse.ArgumentParser(description="Tree making")
 
-args = parser.parse_args()
+    parser.add_argument("-t", "--tree", required=True)
+    parser.add_argument("-n", "--file", required=True)
+    parser.add_argument("-f", "--format", required=True)
+    parser.add_argument("-o", "--output", type=str, default="barchart_tree")
+    parser.add_argument("-s", "--subset", type=str, default="none")
+    parser.add_argument("-m", "--frequency_type", type=str, default="absolute")
 
-subset = args.subset
-frequency_type = args.frequency_type
+    args = parser.parse_args()
 
-subsets = [word.upper() for word in subset.split(",")]  # a list assumed to have two items
-frequency_types = ["absolute", "relative"]
+    subset = args.subset
+    frequency_type = args.frequency_type
 
-all_amino_acids = "ACDEFGHIKLMNPQRSTVWY"
+    subsets = [word.upper() for word in subset.split(",")]  # a list assumed to have two items
+    frequency_types = ["absolute", "relative"]
 
-try:
-    if len(subsets) != 2:
-        raise ValueError("Subsets contain less or more than 2 groupings, make sure to check format!")
-    for subset in subsets:
-        if not set(subset).issubset(set(all_amino_acids)):
-            raise ValueError("Subsets contain invalid amino acid(s), make sure to check spelling!")
-    if frequency_type not in frequency_types:
-        raise ValueError("Invalid tag for frequency type, make sure to check the list of valid tags and spelling!")
-except ValueError as e:
-    print(f"Error: {e}")
-    sys.exit()
+    all_amino_acids = "ACDEFGHIKLMNPQRSTVWY"
 
-taxa_dict = {}  # dictionary to store taxa
-all_seq = ""  # string to hold all the sequences in the alignment
+    try:
+        if len(subsets) != 2:
+            raise ValueError("Subsets contain less or more than 2 groupings, make sure to check format!")
+        for subset in subsets:
+            if not set(subset).issubset(set(all_amino_acids)):
+                raise ValueError("Subsets contain invalid amino acid(s), make sure to check spelling!")
+        if frequency_type not in frequency_types:
+            raise ValueError("Invalid tag for frequency type, make sure to check the list of valid tags and spelling!")
+    except ValueError as e:
+        print(f"Error: {e}")
+        sys.exit()
 
-# read in fasta and parse, then update
-for seq_record in SeqIO.parse(args.file, args.format):
-    new_taxon = Taxon(seq_record.id, seq_record.seq)
-    all_seq += seq_record.seq
-    new_taxon.set_aa_abs_freq()
-    taxa_dict[seq_record.id] = new_taxon  # get taxa dict
+    taxa_dict = {}  # dictionary to store taxa
+    all_seq = ""  # string to hold all the sequences in the alignment
 
-# calculate relative frequencies if specified
-if frequency_type == "relative":
-    # calculate average frequency
-    all_seq = all_seq.replace("-", "")
-    avg_freq_dict = {aa: all_seq.count(aa) / len(all_seq) for aa in all_amino_acids}
+    # read in fasta and parse, then update
+    for seq_record in SeqIO.parse(args.file, args.format):
+        new_taxon = Taxon(seq_record.id, seq_record.seq)
+        all_seq += seq_record.seq
+        new_taxon.set_aa_abs_freq()
+        taxa_dict[seq_record.id] = new_taxon  # get taxa dict
 
-# tree "growing"
-tree = Tree(args.tree)
+    # calculate relative frequencies if specified
+    if frequency_type == "relative":
+        # calculate average frequency
+        all_seq = all_seq.replace("-", "")
+        avg_freq_dict = {aa: all_seq.count(aa) / len(all_seq) for aa in all_amino_acids}
 
-# tree styling
-tree_style = TreeStyle()
-tree_style.show_scale = False  # do not show scale
+    # tree "growing"
+    tree = Tree(args.tree)
 
-
-# layout function
-def layout(node):
-    if node.is_leaf():
-        taxon = taxa_dict[node.name]
-        dict_list = []
-        max_value = 0.2
-
-        if subset == "none":
-
-            if frequency_type == "absolute":
-                dict_list.append(taxon.get_aa_abs_freq())
-            elif frequency_type == "relative":
-                dict_list.append(taxon.get_all_relative_freq(avg_freq_dict))
-                max_value = 0.05
-
-        else:
-            taxon.set_subset_abs_freq(subsets)
-
-            if frequency_type == "absolute":
-                dict_list = taxon.get_subset_abs_freq()
-            elif frequency_type == "relative":
-                dict_list = taxon.get_subset_relative_freq(subsets, avg_freq_dict)
-                max_value = 0.05
-
-        i = 1
-        for freq_dict in dict_list:
-            face = BarChartFace(
-                values=[abs(x) for x in freq_dict.values()],
-                labels=list(freq_dict.keys()),
-                colors=["blue" if f > 0 else "red" for f in freq_dict.values()],
-                width=100,
-                height=50
-            )
-            face.max_value = max_value
-            faces.add_face_to_node(face=face, node=node, column=i, position="aligned")
-            i += 1
+    # tree styling
+    tree_style = TreeStyle()
+    tree_style.show_scale = False  # do not show scale
 
 
-# render tree
-tree.render(args.output + ".png", units="px", h=2000, w=2500, dpi=70,  tree_style=tree_style, layout=layout)
+    # layout function
+    def layout(node):
+        if node.is_leaf():
+            taxon = taxa_dict[node.name]
+            dict_list = []
+            max_value = 0.2
+
+            if subset == "none":
+
+                if frequency_type == "absolute":
+                    dict_list.append(taxon.get_aa_abs_freq())
+                elif frequency_type == "relative":
+                    dict_list.append(taxon.get_all_relative_freq(avg_freq_dict))
+                    max_value = 0.05
+
+            else:
+                taxon.set_subset_abs_freq(subsets)
+
+                if frequency_type == "absolute":
+                    dict_list = taxon.get_subset_abs_freq()
+                elif frequency_type == "relative":
+                    dict_list = taxon.get_subset_relative_freq(subsets, avg_freq_dict)
+                    max_value = 0.05
+
+            i = 1
+            for freq_dict in dict_list:
+                face = BarChartFace(
+                    values=[abs(x) for x in freq_dict.values()],
+                    labels=list(freq_dict.keys()),
+                    colors=["blue" if f > 0 else "red" for f in freq_dict.values()],
+                    width=100,
+                    height=50
+                )
+                face.max_value = max_value
+                faces.add_face_to_node(face=face, node=node, column=i, position="aligned")
+                i += 1
+
+    # render tree
+    tree.render(args.output + ".png", units="px", h=2000, w=2500, dpi=70,  tree_style=tree_style, layout=layout)
