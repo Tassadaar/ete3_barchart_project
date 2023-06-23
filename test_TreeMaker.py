4a3eb0b0d49298da218c903560c3826bd8dd4546
Jason Shao <qz228113@dal.ca>
2023-06-23 14:59:27 -0300

    1st scenario testing complete

--- test_TreeMaker.py
+++ test_TreeMaker.py
@@ -3,11 +3,19 @@ from ete3 import Tree
 from TreeMaker import root
 
 
-# consult scenarios.md for context
+# consult SCENARIOS.md for context
 class Test(TestCase):
+    def test_root_scenario_1(self):
+        input_tree = Tree("input_files/3rd_scenario.tree")
+        input_tree.unroot()
+        result_tree = Tree("input_files/3rd_expected.tree")
+        root(input_tree, ["BRINESHR", "CHICKEN"])
+        rf_distance = input_tree.robinson_foulds(result_tree)[0]
+        self.assertEqual(0, rf_distance, f"The distance should have been 0, but it is {rf_distance}")
+
     def test_root_scenario_3(self):
-        rooted_tree = Tree("input_files/3rd_scenario.tree")
-        des_tree = Tree("input_files/3rd_expected.tree")
-        root(rooted_tree, ["NEMATODE", "LOCUST"])
-        rf_distance = rooted_tree.robinson_foulds(des_tree)[0]
+        input_tree = Tree("input_files/3rd_scenario.tree")
+        result_tree = Tree("input_files/3rd_expected.tree")
+        root(input_tree, ["NEMATODE", "LOCUST"])
+        rf_distance = input_tree.robinson_foulds(result_tree)[0]
         self.assertEqual(0, rf_distance, f"The distance should have been 0, but it is {rf_distance}")
