bf8cbc671df0cda07ce04b1eac1a169c46c327dc
Jason Shao <qz228113@dal.ca>
2023-07-14 12:38:30 -0300

    removed unnecessary optimization concerning seq gaps

--- TreeMaker.py
+++ TreeMaker.py
@@ -37,13 +37,12 @@ def main(args):
     # read in fasta and parse, then update
     for seq_record in SeqIO.parse(args.file, args.format):
         new_taxon = Taxon(seq_record.id, seq_record.seq)
-        all_seq += seq_record.seq
+        all_seq += seq_record.seq.replace("-", "")
         taxa_dict[seq_record.id] = new_taxon  # get taxa dict
 
     # calculate relative frequencies if specified
     if frequency_type == "relative" or chi2_score is True:
         # calculate average frequency
-        all_seq = all_seq.replace("-", "")
         avg_freq_dict = {aa: all_seq.count(aa) / len(all_seq) for aa in all_amino_acids}
 
     # tree rooting
