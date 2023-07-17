d142f52ee893cfb42769e4df1f492b4fc21aff75
Joran Martijn <joranmartyn@gmail.com>
2023-07-17 15:38:14 -0300

    Taxon class joran refactor

--- Taxon.py
+++ Taxon.py
@@ -10,17 +10,20 @@ class Taxon:
     def __init__(self, name, seq):
         self.name = name
         self.seq = seq.replace("-", "")
-        self.freq_dict = {aa: self.seq.count(aa) / len(self.seq) for aa in self.all_amino_acids}
+        self.abs_freq_dict = {aa: self.seq.count(aa) / len(self.seq) for aa in self.all_amino_acids}
+        self.display_freqs = {}
         self.group1_freq = {}
         self.group2_freq = {}
         self.other_freq = {}
+        self.group1_rel_freq = {}
+        self.group2_rel_freq = {}
+        self.other_rel_freq = {}
 
     def get_all_relative_freq(self, avg_freq_dict):
-        return {aa: self.freq_dict[aa] - avg_freq for aa, avg_freq in avg_freq_dict.items()}
+        return {aa: self.abs_freq_dict[aa] - avg_freq for aa, avg_freq in avg_freq_dict.items()}
 
     def set_subset_abs_freq(self, subsets):
-
-        for key, value in self.freq_dict.items():
+        for key, value in self.abs_freq_dict.items():
             if key in subsets[0]:
                 self.group1_freq[key] = value
             elif key in subsets[1]:
@@ -31,20 +34,26 @@ class Taxon:
     def get_subset_abs_freq(self):
         return [self.group1_freq, self.group2_freq, self.other_freq]
 
-    def get_subset_relative_freq(self, subsets, avg_freq_dict):
-        group1_rel_freq = {}
-        group2_rel_freq = {}
-        other_rel_freq = {}
-
+    def set_subset_rel_freq(self, subsets, avg_freq_dict):
         for aa, avg_freq in avg_freq_dict.items():
             if aa in subsets[0]:
-                group1_rel_freq[aa] = self.group1_freq[aa] - avg_freq
+                # group1_rel_freq[aa] = self.group1_freq[aa] - avg_freq
+                self.group1_rel_freq[aa] = self.group1_freq[aa] - avg_freq
             elif aa in subsets[1]:
-                group2_rel_freq[aa] = self.group2_freq[aa] - avg_freq
+                # group2_rel_freq[aa] = self.group2_freq[aa] - avg_freq
+                self.group2_rel_freq[aa] = self.group2_freq[aa] - avg_freq
             else:
-                other_rel_freq[aa] = self.other_freq[aa] - avg_freq
+                # other_rel_freq[aa] = self.other_freq[aa] - avg_freq
+                self.other_rel_freq[aa] = self.other_freq[aa] - avg_freq
 
-        return [group1_rel_freq, group2_rel_freq, other_rel_freq]
+    def get_subset_rel_freq(self):
+        return [self.group1_rel_freq, self.group2_rel_freq, self.other_rel_freq]
+
+    def set_display_freqs(self, mode, avg_freq_dict=None):
+        if mode == 'absolute':
+            self.display_freqs = self.abs_freq_dict
+        elif mode == 'relative':
+            self.display_freqs = self.get_all_relative_freq(avg_freq_dict)
 
     def calculate_chi_square(self, align_freqs):
         chi_square_score = 0
@@ -52,7 +61,7 @@ class Taxon:
 
         for aa in self.all_amino_acids:
             expected_count = align_freqs[aa] * taxon_seq_len
-            observed_count = self.freq_dict[aa] * taxon_seq_len
+            observed_count = self.abs_freq_dict[aa] * taxon_seq_len
             chi_square_score += (observed_count - expected_count) ** 2 / expected_count
 
         return round(chi_square_score, 1)
